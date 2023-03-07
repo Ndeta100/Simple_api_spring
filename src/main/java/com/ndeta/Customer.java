045eaa81266e6a00989c55a062c418577ccc5752
ndeta <ndetainnocent3@gmail.com>
2023-03-07 20:55:49 +0200

    Initial commit

---
+++ src/main/java/com/ndeta/Customer.java
@@ -0,0 +1,70 @@
+package com.ndeta;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private int age;
+
+    public Customer(Integer id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/ndeta/Main.java
@@ -0,0 +1,19 @@
+package com.ndeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/")
+    public  GreetResponse greet(){
+        return new GreetResponse("<h1>Hello</h1>");
+    }
+    record GreetResponse(String greet){}
+}
